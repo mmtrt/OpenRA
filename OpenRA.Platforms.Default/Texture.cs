dd2cbb61e141b57cfc543e029cbdd64eebb7f21c
тαqι <mttalpur@gmail.com>
2026-08-09 03:30:15 +0500

    Update Texture.cs

--- OpenRA.Platforms.Default/Texture.cs
+++ OpenRA.Platforms.Default/Texture.cs
@@ -72,12 +72,10 @@ namespace OpenRA.Platforms.Default
 		void SetData(IntPtr data, int width, int height)
 		{
 			PrepareTexture();
-			var glInternalFormat = OpenGL.Profile == GLProfile.Embedded
-			? OpenGL.GL_BGRA8_EXT
-			: OpenGL.GL_RGBA8;
+			var glInternalFormat = OpenGL.Profile == GLProfile.Embedded ? OpenGL.GL_BGRA8_EXT : OpenGL.GL_RGBA8;
 
 			OpenGL.glTexImage2D(OpenGL.GL_TEXTURE_2D, 0, glInternalFormat, width, height,
-								0, OpenGL.GL_BGRA, OpenGL.GL_UNSIGNED_BYTE, data);
+				0, OpenGL.GL_BGRA, OpenGL.GL_UNSIGNED_BYTE, data);
 			OpenGL.CheckGLError();
 		}
 
@@ -122,12 +120,10 @@ namespace OpenRA.Platforms.Default
 
 			PrepareTexture();
 
-			var glInternalFormat = OpenGL.Profile == GLProfile.Embedded
-			? OpenGL.GL_BGRA8_EXT
-			: OpenGL.GL_RGBA8;
+			var glInternalFormat = OpenGL.Profile == GLProfile.Embedded ? OpenGL.GL_BGRA8_EXT : OpenGL.GL_RGBA8;
 
 			OpenGL.glCopyTexImage2D(OpenGL.GL_TEXTURE_2D, 0, glInternalFormat,
-									rect.X, rect.Y, rect.Width, rect.Height, 0);
+				rect.X, rect.Y, rect.Width, rect.Height, 0);
 			OpenGL.CheckGLError();
 		}
 
